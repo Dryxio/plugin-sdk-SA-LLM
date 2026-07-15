6baf0948d743b8d815c0b68d14617f1b626ada04
Dryxio <momemtumcorpfzco@gmail.com>
2026-07-15 10:48:05 +0200

    Complete SA task sequence layout

--- plugin_sa/game_sa/CTaskComplexSequence.h
+++ plugin_sa/game_sa/CTaskComplexSequence.h
@@ -13,8 +13,23 @@ class CTaskComplexSequence : public CTaskComplex {
 protected:
     CTaskComplexSequence(plugin::dummy_func_t a) : CTaskComplex(a) {}
 public:
+    int m_nCurrentTaskIndex;
+    CTask *m_apTasks[8];
+    unsigned int m_nRepeatMode;
+    int m_nRepeatedCount;
+    bool m_bFlushTasks;
+    unsigned char gap39[3];
+    unsigned int m_nReferenceCount;
+
     CTaskComplexSequence();
     ~CTaskComplexSequence();
     bool AddTask(CTask *pTask);
 };
-VALIDATE_SIZE(CTaskComplexSequence, 0xC);
+VALIDATE_OFFSET(CTaskComplexSequence, m_nCurrentTaskIndex, 0xC);
+VALIDATE_OFFSET(CTaskComplexSequence, m_apTasks, 0x10);
+VALIDATE_OFFSET(CTaskComplexSequence, m_nRepeatMode, 0x30);
+VALIDATE_OFFSET(CTaskComplexSequence, m_nRepeatedCount, 0x34);
+VALIDATE_OFFSET(CTaskComplexSequence, m_bFlushTasks, 0x38);
+VALIDATE_OFFSET(CTaskComplexSequence, gap39, 0x39);
+VALIDATE_OFFSET(CTaskComplexSequence, m_nReferenceCount, 0x3C);
+VALIDATE_SIZE(CTaskComplexSequence, 0x40);
